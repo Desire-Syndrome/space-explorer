cc975147eba0f9bfec806b5a28a410a92bf259f1
Desire-Syndrome <desiresyndrome@gmail.com>
2025-06-16 13:37:36 +0100

    Test git

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const connectDB = require('./configs/db.js');
 
   
-// setup Middleware
+// setup Middleware//
 const app = express();
 app.use(cors());
 app.use(express.json());
