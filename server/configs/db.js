a4429a38ff7c0b994a0a964882023a1dc69a404b
Desire-Syndrome <desiresyndrome@gmail.com>
2025-06-24 00:36:56 +0100

    1

--- server/configs/db.js
+++ server/configs/db.js
@@ -4,10 +4,10 @@ const connectDB = async () => {
   try {
     const MONGODB_URL = process.env.MONGODB_URL;
     
-    if (process.env.NODE_ENV !== 'test') {
+   
       await mongoose.connect(MONGODB_URL)
       console.log("DB connected");
-    }
+ 
   } catch (error) {
     console.log("DB connection failed:", error);
   }
